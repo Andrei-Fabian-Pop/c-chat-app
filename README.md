73ba8db4a97bd1911a55837be6abd546d13550c3
916_Pop_Andrei_Fabian <andrei.fabian.pop@stud.ubbcluj.ro>
2022-08-27 15:09:17 +0300

    added SIGINT handler

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# c-chat-app
+# Chat Application
+
+### C++ Chat Console application using C++17 and boost::asio asynchronous library(https://www.boost.org/doc/libs/1_62_0/doc/html/boost_asio.html). The project generates 2 executables (./server and ./client). The server app retains the message history(default: last 100 messages) so that they can be broadcast to every new joined user. Ctrl+C is used to exit the apps.
+
+## Server architecture
+
+- The server app user the Observer design pattern to update all participants.
+- The first received message is the name of the new user (stored in a map)
+- The Participant class represents the observer in the model and the Room class represents the subject
+- The Session class is responsible with connecting the Server with the client (Participant)
+
+## Usage:
+
+- Client app
+  - `./client` - starts the executable, then it prompts for the ip, port number and name
+  - `./client <ip address> <port number>` -  start the executable with the ip and port as arguments, the programme will prompt user to enter name
+  - `./client <ip address> <port number> <name>` - all variables are specified as arguments
+- Server app
+  - `./server <port number>`
--- src/Client/Client_UI.cpp
+++ src/Client/Client_UI.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <thread>
+#include <csignal>
 
 #include <boost/asio.hpp>
 #include <utility>
@@ -19,8 +20,26 @@ private:
     boost::asio::ip::tcp::resolver resolver_;
     boost::asio::ip::tcp::resolver::iterator endpoint_iterator_;
 
+    std::unique_ptr<Client> c;
+
+    static void exitHandler([[maybe_unused]] int signal) {
+        std::cout << "\nSIGINT called\n";
+    }
+
+    static void addSigInt() {
+        struct sigaction sigIntHandler{};
+
+        sigIntHandler.sa_handler = exitHandler;
+        sigemptyset(&sigIntHandler.sa_mask);
+        sigIntHandler.sa_flags = 0;
+
+        sigaction(SIGINT, &sigIntHandler, nullptr);
+    }
+
 public:
     explicit UI() : resolver_(this->io_service_) {
+        addSigInt();
+
         std::cout << "Welcome!!\n";
         std::cout << "Enter your name:";
         std::cin >> this->name_;
@@ -35,6 +54,8 @@ public:
     }
 
     explicit UI(std::string ip, std::string port) : resolver_(this->io_service_), ip_(std::move(ip)), port_(std::move(port)) {
+        addSigInt();
+
         std::cout << "Welcome!!\n";
         std::cout << "Enter your name:";
         std::cin >> this->name_;
@@ -42,8 +63,18 @@ public:
         this->endpoint_iterator_ = boost::asio::ip::tcp::resolver::iterator(this->resolver_.resolve(this->ip_, this->port_));
     }
 
+    explicit UI(std::string ip, std::string port, std::string name) : resolver_(this->io_service_), ip_(std::move(ip)), port_(std::move(port)), name_(std::move(name)) {
+        addSigInt();
+
+        this->endpoint_iterator_ = boost::asio::ip::tcp::resolver::iterator(this->resolver_.resolve(this->ip_, this->port_));
+    }
+
+    ~UI() {
+        c->close();
+    }
+
     void run() {
-        Client c(this->io_service_, this->endpoint_iterator_, this->name_);
+        c = std::make_unique<Client>(this->io_service_, this->endpoint_iterator_, this->name_);
 
         std::thread t([&] { this->io_service_.run(); });
 
@@ -53,23 +84,25 @@ public:
             msg.body_length(std::strlen(line));
             std::memcpy(msg.body(), line, msg.body_length());
             msg.encode_header();
-            c.write(msg);
+            c->write(msg);
         }
 
-        c.close();
+        c->close();
         t.join();
     }
 };
 
 auto main(int argc, char** argv) -> int {
     try {
-        if (argc != 1) {
-            UI ui(argv[1], argv[2]);
-            ui.run();
+        std::unique_ptr<UI> ui;
+        if (argc == 3) {
+            ui = std::make_unique<UI>(argv[1], argv[2]);
+        } else if (argc == 4) {
+            ui = std::make_unique<UI>(argv[1], argv[2], argv[3]);
         } else {
-            UI ui;
-            ui.run();
+            ui = std::make_unique<UI>();
         }
+        ui->run();
     } catch (std::exception &e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
